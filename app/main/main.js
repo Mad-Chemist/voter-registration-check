6f01417bc1d30319f74d814a4f5d39943b8cfba6
Vico Bertogli <[mac]>
2016-05-01 13:11:12 -0400

    Better experience around displaying voter status

--- app/main/main.js
+++ app/main/main.js
@@ -11,26 +11,26 @@ const seleniumRunner = require('./run-selenium.js');
 var window = null; // Keep a global reference of the window object, if you don't, the window will be closed automatically when the JavaScript object is garbage collected.
 
 app.on('window-all-closed', function() { // Quit when all windows are closed.
-  // On OS X it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+	// On OS X it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('ready', function() {
-  window = new BrowserWindow({ frame: false, resizable: false, width: 800, height: 600 }); // Create the browser window.
-  window.loadURL('file://' + __dirname + '/../render/index.html'); // and load the index.html of the app.
-  // window.webContents.openDevTools(); // Open the DevTools.
+	window = new BrowserWindow({ frame: false, resizable: false, width: 800, height: 600 }); // Create the browser window.
+	window.loadURL('file://' + __dirname + '/../render/index.html'); // and load the index.html of the app.
+	// window.webContents.openDevTools(); // Open the DevTools.
 
-  electron.ipcMain.on('close-window', window.close)
-    .on('save-voter', function(event, voterInfo) {
-      function callback(ch, ms) {
-        event.sender.send(ch, ms);
-      }
-      seleniumRunner.init(null, voterInfo, callback);
-    });
+	electron.ipcMain.on('close-window', window.close)
+		.on('save-voter', function(event, voterInfo) {
+			function callback(ch, ms) {
+				event.sender.send(ch, ms);
+			}
+			seleniumRunner.init(null, voterInfo, callback);
+		});
 
-  window.on('closed', function() {
-    window = null;
-  });
+	window.on('closed', function() {
+		window = null;
+	});
 });
---
+++ app/render/index.js
@@ -0,0 +1,74 @@
+var LS_VOTER = 'voter';
+var $ = require('jquery');
+var electron = require('electron');
+
+$(document).ready(function() {
+	function prePopuplate() {
+		if (localStorage[LS_VOTER].length > 0) {
+			var dataObj = JSON.parse(localStorage[LS_VOTER]);
+			Object.keys(dataObj).forEach(function(key) {
+				if ($('#' + key)) {
+					$('#' + key).val(dataObj[key]);
+				}
+			});
+		}
+	}
+
+	function formValidation() {
+		var passes = true;
+		$('form').find('select, input').each(function() {
+			$(this).css('border', '');
+			if ($(this).val() === "" || $(this).is('#birthdate') && $(this).val().split('/').length !== 3) {
+				passes = false
+				$(this).css('border', '1px red solid');
+			}
+		});
+		return passes;
+	}
+
+	function extractFormObject() {
+		var data = {};
+		
+		$('form').find('select, input').each(function() {
+			data[$(this).attr('id')] = $(this).val();
+		});
+
+		localStorage[LS_VOTER] = JSON.stringify(data);
+		return data;
+	}
+
+	function displayVoterStatus(event, status) {
+		var formData = extractFormObject();
+
+		var $container = $('.container-fluid');
+		var message = status ? "registered" : "not registered";
+		var className = status ? "success" : "error";
+
+		var template = `
+			<div id="resolution" class="middled panel text-center panel-${className}">
+				<div class="panel-heading">
+					<h3 class="panel-title">${formData.party} registration status</h3>
+				</div>
+				<div class="panel-body">
+					${formData.firstName} ${formData.lastName} is ${message} ${formData.party}.
+				</div>
+			</div>
+		`;
+		
+		$container.find('form').hide();
+		$container.find('#resolution').remove();
+		$container.append(template);
+	}
+
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		if (formValidation() === true) {
+			$(':focus').blur();
+			$('form').addClass('disabled');
+			electron.ipcRenderer.send('save-voter', extractFormObject());
+		}
+	});
+
+	electron.ipcRenderer.on('display-status', displayVoterStatus);
+	prePopuplate();
+});
