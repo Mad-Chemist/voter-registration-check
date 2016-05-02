fb3120fe95f15085d055a1bb865169239f1e01f1
Vico Bertogli <[mac]>
2016-05-01 23:55:29 -0400

    And Montana before bed

---
+++ app/main/states/montana.js
@@ -0,0 +1,38 @@
+const Q = require('q');
+const URL = "https://app.mt.gov/cgi-bin/voterinfo/voterinfo.cgi";
+const INFO = {
+	'firstName':'#firstName',
+	'lastName':'#lastName',
+	'birthdate':'#dob',
+	'proceedBTN':'input[type="submit"]',
+	'submit':'input.beginButton[type="submit"]',
+	'status':'#mainContentIndex > p:nth-child(3)',
+	'expectedStatus':'VOTER STATUS: Active'
+};
+
+var user = {
+	"firstName":"Linda",
+	"lastName":"McCulloch",
+	"birthdate":"12/21/1954"
+};
+
+var verifyRegistration = function(client, user) {
+	return Q.promise(function(resolve, reject) {
+		client
+			.url(URL)
+			.click(INFO['proceedBTN'])
+			.setValue(INFO['firstName'], user['firstName'])
+			.setValue(INFO['lastName'], user['lastName'])
+			.setValue(INFO['birthdate'], user['birthdate'])
+			.click(INFO['submit'])
+			.waitForExist(INFO['status'], 5000)
+			.getText(INFO['status'])
+				.then(function(value) {
+					resolve(INFO['expectedStatus'] === value.trim());
+				});
+	});
+};
+
+module.exports = {
+	verifyRegistration:verifyRegistration
+};
