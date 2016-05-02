642519dac0473c2fbc8d70f2c788f7ac43f91f6d
Vico Bertogli <[mac]>
2016-05-02 08:33:57 -0400

    Merged @ThePatShea's kentucky script

---
+++ app/main/states/kentucky.js
@@ -0,0 +1,34 @@
+const Q = require('q');
+
+const INFO = {
+	'status':'[name="ELIGIBLE FOR PRIMARY"]',
+	'expectedStatus':'Yes'
+};
+
+var user = {
+	"firstName":"Alison",
+	"lastName":"Grimes",
+	"birthdate":"11/23/1978"
+};
+
+var fullUrl = 'https://vrsws.sos.ky.gov/vic/Default.aspx';
+fullUrl += '?FIRST_NAME=' + user.firstName;
+fullUrl += '&MIDDLE_NAME=&LAST_NAME=' + user.lastName;
+fullUrl += '&DATE_OF_BIRTH=' + encodeURIComponent(user.birthdate);
+fullUrl += '&SSN_LAST4=&submit=Submit';
+
+
+var verifyRegistration = function(client, user) {
+	return Q.promise(function(resolve, reject) {
+		client
+			.url(fullUrl)
+			.getValue(INFO['status'])
+				.then(function(value) {
+					resolve(INFO['expectedStatus'] === value.trim());
+				});
+	});
+};
+
+module.exports = {
+	verifyRegistration:verifyRegistration
+};
