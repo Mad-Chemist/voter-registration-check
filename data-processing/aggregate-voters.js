2ab0f9171dedb73397c5dc6e6663a6036e416742
Pat Shea <patshea56@gmail.com>
2016-05-02 23:27:13 -0700

    connected to mLab database

--- data-processing/aggregate-voters.js
+++ data-processing/aggregate-voters.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var mongoose = require('mongoose');
 require('./models/rawPoliticalStrategies');
-mongoose.connect('mongodb://localhost/voters');
+mongoose.connect('mongodb://thepatshea:eI4bt%n3l5^m8w@ds013162.mlab.com:13162/voterregistrationmonitordb');
 
 
 
@@ -30,7 +30,13 @@ var makeRawPoliticalStrategy = function(inputHtml, inputVoterNum) {
 var scrapePoliticalStrategies = function(voterNum, callback) {
     request('http://www.politicalstrategies.com/voter/' + voterNum, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            callback(body, voterNum);
+            // var regExHtml = new RegExp('<div class="outerwrapper">(.*?)<script', 'g');
+            var regExHtml = new RegExp('outerwrapper(.*?)page', 'g'); // Working on this
+            var inputHtml = regExHtml.exec(body);
+
+            console.log(inputHtml);
+
+            //callback(inputHtml, voterNum);
         }
     });
 };
@@ -43,7 +49,10 @@ var scrapeAllPoliticalStrategies = function(firstVoter, lastVoter) {
 }
 
 // Max is around 57000000, around half the total number of registered voters in the US
-scrapeAllPoliticalStrategies(1,100000);
+scrapeAllPoliticalStrategies(1,1);
 
 
 // https://iservices.intelius.com/premier/search.php?componentId=1&qf=Anthony&qn=Erjavec&qcs=Cleveland%2C+OH
+// http://www.politicalstrategies.com/voter/57000000
+// http://mlab.com
+// @TODO: Add some regex so it doesn't save every single thing on the html page. It's taking up too much space.
