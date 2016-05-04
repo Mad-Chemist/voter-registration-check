c8057340577135fd21e718c1095e62c4ae8989f0
Vico Bertogli <[mac]>
2016-05-03 22:23:00 -0400

    Building installer & apps more effectively
    Apps are now using specified iconography

--- build.js
+++ build.js
@@ -4,13 +4,15 @@
 var options = {
 	'all':true,
 	'overwrite':true,
+	'appName':'Voter-Registration-Check',
 	'name':'Voter-Registration-Check',
 	'dir':'./app',
 	'out':'./dist',
 	'arch':'all',
 	'platform':'all',
 	'asar':true,
-	'icon':'./build',
+	'icons':'./build/',
+	'ignore':'',
 	"app-bundle-id":"css.expert.prch",
 	"app-category-type":"public.app-category.utilities"
 };
---
+++ electron-build.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const appJson = require('./app/package.json');
+const builder = require("electron-builder");
+const options = {
+	"verison":appJson.version,
+	"platform": "all",
+	"asar":true,
+	"productName": appJson.name,
+	"app-bundle-id": "css.expert.prch",
+	"app-category-type": "public.app-category.utilities",
+	"out":"./dist"
+
+};
+
+// Promise is returned
+var build = builder.build(options)
+	.then(() => {
+		console.log('------------');
+		console.log('Built ' + appJson.name + ' V' + appJson.version);
+		console.log('------------');
+
+	})
+	.catch((error) => {
+		console.log('------------');
+		console.error(error);
+		console.log('------------')
+	});
