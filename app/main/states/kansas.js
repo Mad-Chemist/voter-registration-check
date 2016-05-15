9be595d877abf865697065e35825989e5e8fc609
Nyefan <nyefancoding@gmail.com>
2016-05-15 11:53:56 -0500

    Add county to Kansas.js.  Bring Kansas.js user in line with the standard

--- app/main/states/kansas.js
+++ app/main/states/kansas.js
@@ -1,41 +1,41 @@
 const Q = require('q');
 const URL = "https://myvoteinfo.voteks.org/VoterView/RegistrantSearch.do"
 const INFO = {
-	'firstName':'#nameFirst',
-	'lastName':'#nameLast',
-	'dobMonth':'#dobMonth',
-	'dobDay':'#dobDay',
+        'firstName':'#nameFirst',
+        'lastName':'#nameLast',
+        'dobMonth':'#dobMonth',
+        'dobDay':'#dobDay',
         'dobYear':'#dobYear',
         'submit':'input#search',
-	'status':'div#registrant > p:nth-child(4)',
-	'expectedStatus':'Active'
+        'status':'div#registrant > p:nth-child(4)',
+        'expectedStatus':'Active'
 };
 
 var user = {
+        'county':'Sedgwick',
         'firstName':'Anthony',
         'lastName':'St Aubin',
-        'dobMonth':'Sep',
-        'dobDay':'09',
-        'dobYear':'1993',
+        'birthday':'09/09/1993'
         'expectedParty':'Democratic'
 };
 
 var verifyRegistration = function(client, user) {
-	return Q.promise(function(resolve, reject) {
-		client
-			.url(URL)
-			.setValue(INFO['firstName'], user['firstName'])
-			.setValue(INFO['lastName'], user['lastName'])
-			.setValue(INFO['dobMonth'], user['dobMonth'])
-                        .setValue(INFO['dobYear'], user['dobYear'])
-                        .setValue(INFO['dobDay'], user['dobDay'])
-			.click(INFO['submit'])
-			.waitForExist(INFO['status'], 5000)
-			.getText(INFO['status'])
-			.then(function(value) {
-				resolve(INFO['expectedStatus'] === value);
-			});
-	});
+        return Q.promise(function(resolve, reject) {
+                client
+                        .url(URL)
+                        .selectByVisibleText(INFO['county'], user['county'])
+                        .setValue(INFO['firstName'], user['firstName'])
+                        .setValue(INFO['lastName'], user['lastName'])
+                        .selectByValue(INFO['dobMonth'], user['birthday'].slice(0,1))
+                        .selectByValue(INFO['dobYear'], user['birthday'].slice(3, 4))
+                        .selectByValue(INFO['dobDay'], user['birthday'].slice(6, 9))
+                        .click(INFO['submit'])
+                        .waitForExist(INFO['status'], 5000)
+                        .getText(INFO['status'])
+                        .then(function(value) {
+                                resolve(INFO['expectedStatus'] === value);
+                        });
+        });
 };
 
 module.exports = {
